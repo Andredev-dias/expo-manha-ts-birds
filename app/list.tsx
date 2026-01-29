0e22badecc6f8f2005f0e3c0a4212bfbd2cb63f9
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2026-01-29 11:27:01 -0300

    Add files via upload

--- app/list.tsx
+++ app/list.tsx
@@ -1,15 +1,42 @@
 import { Header } from "@/components/header";
-import { View, Text } from "react-native";
+import { router } from "expo-router";
+import { View, Text, TouchableOpacity, StyleSheet, FlatList, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import dados from '@/assets/constants/mock'
+import { Item } from "@/components/item";
 
 export default function List() {
+
+    const voltar = () => {
+        router.push('/')
+    }
+
     return (
-        <SafeAreaView>
-            <Header image={require('@/assets/images/headerImg.jpg')} />
+     <>
+        <Header image={require('@/assets/images/headerImg.jpg')} />
             <View>
-                <Text>TESTE</Text>
+                <TouchableOpacity onPress={voltar}>
+                    <Text style={s.backBtn}>🔙</Text>
+                </TouchableOpacity>
             </View>
+        
+        <SafeAreaView style={s.wrapList}>
+                <FlatList data={dados} renderItem={({ item }) => (
+                    <Item name={item.nome} image={item.imagem} date={item.nascimento} age={item.idade} />
+                )}>
+                </FlatList>
         </SafeAreaView>
+        </>
     )
 }
+
+const s = StyleSheet.create({
+    wrapList:{
+        flex: 1
+    },
+    backBtn: {
+        fontSize: 40,
+        position: 'absolute',
+        top: -250
+    }
+})
---
+++ components/item.tsx
@@ -0,0 +1,66 @@
+import { View, Text, StyleSheet } from "react-native"
+import { Image } from 'expo-image'
+
+export const Item = ({ name, age, date, image }: { name: string, age: string, date: string, image: string }) => {
+    return (
+        <View style={s.itemList}>
+            <Image style={s.imageStyle} source={image} />
+            <Text style={s.txt}>{name}</Text>
+            <Text style={s.txt2}>Idade: {age}</Text>
+            <Text style={s.txt3}>{date}</Text>
+        </View>
+    )
+}
+
+const s = StyleSheet.create({
+    itemList: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 20,
+        backgroundColor: '#4186a4d4',
+        margin: 10,
+        height: 80,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+        elevation: 10,
+    },
+    imageStyle: {
+        width: 70,
+        height: 70,
+        borderRadius: '100%',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+        elevation: 10,
+    },
+    txt: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textShadowColor: 'rgba(0, 0, 0, 0.75)', 
+        textShadowOffset: { width: -2, height: 2 }, 
+        textShadowRadius: 10, 
+    },
+    txt2: {
+        color: '#093753c3',
+        fontSize: 14,
+        fontWeight: 'bold'
+    },
+    txt3: {
+        color: '#fff',
+        fontSize: 14,
+        fontWeight: 'regular'
+    }
+})
